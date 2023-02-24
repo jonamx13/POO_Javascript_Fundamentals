b5033f8926ce25e649f734b8004f409d94629b5d
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-02-24 10:28:03 -0600

    Challenge: Add Learning Paths Class

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+class LearningPaths {
+    constructor({
+        pathName,
+        courses = [],
+    }) {
+        this.pathName = pathName;
+        this.courses = courses;
+    }
+}
+
+const escuelaWeb = new LearningPaths({
+    pathName: 'Escuela de Desarrollo web',
+    
+    courses: [
+        'Curso Definitivo de HTML y CSS',
+        'Curso Práctico de HTML y CSS',
+        'Curso de Frontend Developer',
+    ],
+});
+
+const escuelaData = new LearningPaths({
+    pathName: 'Escuela de Data Science',
+    
+    courses: [
+        'Curso Introductorio de Python',
+        'Curso Básico de MySQL',
+        'Curso de Análisis de Datos con Python',
+    ],
+});
+
+const escuelaVgs = new LearningPaths({
+    pathName: 'Escuela de Videojuegos',
+    
+    courses: [
+        'Curso Introductorio al Desarrollo de Videojuegos',
+        'Curso de Diseño de Personajes',
+        'Curso de C# para Unity'
+    ],
+});
+
 class Student {
     constructor({
         name,
@@ -27,6 +67,10 @@ const jona2 = new Student({
     username: 'JonaMx13',
     email: 'jonathan@meixueiro.com',
     twitter: 'jonamx13',
+    learningPaths: [
+        escuelaWeb,
+        escuelaVgs
+    ],
 });
 
 const miguelito = new Student({
@@ -34,4 +78,8 @@ const miguelito = new Student({
     username: 'chanchitofeliz',
     email: 'miguelito@miguelon.com',
     instagram: 'miguelito_chanchito',
+    learningPaths: [
+        escuelaWeb,
+        escuelaData,
+    ],
 });
